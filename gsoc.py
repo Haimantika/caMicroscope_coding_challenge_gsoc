c201ba4936a018ef7d2e9d66307e16c1632eac48
haimantika mitra <32809211+Haimantika@users.noreply.github.com>
2020-03-24 19:22:51 +0530

    Update gsoc.py

--- gsoc.py
+++ gsoc.py
@@ -61,3 +61,20 @@ while(1):
 
 cv2.destroyAllWindows()
 cap.release()
+
+
+#for nearest value
+import numpy as np
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx]
+
+array = np.random.random(10)
+print(array)
+# [ 0.21069679  0.61290182  0.63425412  0.84635244  0.91599191  0.00213826
+#   0.17104965  0.56874386  0.57319379  0.28719469]
+
+value = 0.5
+
+print(find_nearest(array, value))
